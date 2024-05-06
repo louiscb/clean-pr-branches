e17a573981ab3e51e46b689e5d645e53744ed400
Louis Cameron Booth <me@louiscb.com>
2024-05-06 17:28:49 +0200

    Update readme

--- README.md
+++ README.md
@@ -9,9 +9,9 @@ It deletes the branches associated with merged or closed pull requests.
 
 ## How to use
 
-In your chosen repository, run the script passing in the date you want the pull requests to be older than. E.g this will delete the branches of closed or merged PRs created before 2024.
+In your chosen repository run the script.
 
 ```
-./delete_branches.sh 2024-01-01
+./delete_branches.sh
 ```
 
--- delete_branches.sh
+++ delete_branches.sh
@@ -11,25 +11,13 @@ cleanup() {
 
 trap cleanup SIGINT SIGTERM
 
-# Check if the date argument is provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <date>"
-    exit 1
-fi
-
-date_arg=$1
-
 # Run the command to get the list of branch names
 mkdir $dir
 
 echo "Fetching branches from Github origin"
 
-gh pr list --state merged --search "created:<$date_arg" --json headRefName -L 100000 -q '.[] | .headRefName' > $dir/branches.txt
-gh pr list --state closed --search "created:<$date_arg" --json headRefName -L 100000 -q '.[] | .headRefName' >> $dir/branches.txt
-
-line_count=$(wc -l < "$file_name")
-
-echo "Found $line_count branches to delete."
+gh pr list --state merged --json headRefName -L 100000000 -q '.[] | .headRefName' > $dir/branches.txt
+gh pr list --state closed --json headRefName -L 100000000 -q '.[] | .headRefName' >> $dir/branches.txt
 
 delete_branch() {
     echo "Processing: $1"
